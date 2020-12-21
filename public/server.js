040ebf02ba3fdfa49965cdd424d06328314beabd
mateotrujillo16 <mateotrujillo1114@gmail.com>
2020-12-21 13:54:02 -0500

    added the api functionality and routes for server

---
+++ public/server.js
@@ -0,0 +1,76 @@
+
+
+const express = require('express');
+const path = require('path');
+const bcrypt = require('bcrypt');
+const { Server } = require('http');
+
+const users = [];
+const app = express();
+
+app.use(express.urlencoded({ extended: false })) //tells the application to take the login forms and be able to access them in our request variables inside the post method
+app.use(express.static(path.join(__dirname, './public')));
+const port = process.env.port || 6060;
+
+// API space (coming)
+
+
+// request(url, (err, res, body) => {
+//   if (err) {
+//     console.log("err", err);
+//   } else {
+//     console.log("body", body)
+//   }
+// })
+
+
+////// LOGIN /////
+
+app.get('/', (req, res) => {
+  res.sendFile('/index.html', { root: __dirname })
+});
+
+app.get('/login', (req, res) => {
+  res.sendFile('/login.html', { root: __dirname })
+});
+
+app.get('/register', (req, res) => {
+  res.sendFile('/register.html', { root: __dirname })
+});
+
+app.get('/about', (req, res) => {
+  res.sendFile('/login.html', { root: __dirname })
+});
+
+
+app.post('/login', (req, res) => {
+  
+  const username = req.body.username;
+  const password = req.body.password;
+  if (users.username !== username) {
+    res.redirect('/login');
+  } else if (users.password !== password){
+    res.redirect('/login')
+  } else {
+    res.redirect('/')
+  }
+})
+
+app.post('/register', async (req, res) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10)
+    users.push({
+      id: Date.now().toString(),
+      name: req.body.name,
+      email: req.body.email,
+      username: req.body.username,
+      password: hashedPassword
+    })
+    res.redirect('/login')
+  } catch {
+    res.redirect('/register')
+  }
+  console.log(users);
+})
+
+app.listen(port)
